c6f8fd114d984df8c39eb560deb548f922e2a572
mraveragedot <nohlgustafson05@gmail.com>
2024-04-09 09:53:41 -0600

    started

--- project.01/sketch.js
+++ project.01/sketch.js
@@ -1,13 +1,45 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// music ear trainer
+// Nohl G
+// April 9th 2024
 
-
+notes = new Map([
+  //FIRST STRING
+  ["G", 500],
+  ["G#", 500],
+  ["A", 500],
+  ["A#", 500],
+  ["B", 300],
+  ["C", 200],
+  ["C#", 200],
+  //SECOND STRING
+  ["D", 500],
+  ["D#", 500],
+  ["E", 300],
+  ["F", 200],
+  ["F#", 200],
+  ["G", 500],
+  ["G#", 500],
+  //THIRD STRING
+  ["A", 500],
+  ["A#", 500],
+  ["B", 300],
+  ["C", 200],
+  ["C#", 200],
+  ["D", 500],
+  ["D#", 500],
+  //FORTH STRING
+  ["E", 300],
+  ["F", 200],
+  ["F#", 200],
+  ["G", 500],
+  ["G#", 500],
+  ["A", 500],
+  ["A#", 500],
+  
+]);
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
 }
 
 function draw() {
